2ed7433ec41d0bbec4063c62a986daa078f0dae8
Jalilov <javohirjalilov777@gmail.com>
2022-10-25 14:59:00 +0500

    send weather city

---
+++ bot.py
@@ -0,0 +1,42 @@
+from ctypes import resize
+import requests
+import os
+import weather_api
+TOKEN = os.environ['TOKEN']
+key = os.environ['KEY']
+def get_updates(TOKEN):
+    updates = requests.get(f'https://api.telegram.org/bot{TOKEN}/getUpdates')
+    updates = updates.json()
+    return updates
+
+def get_last_update(updates):
+    last_updates = updates['result'][-1]
+    chat_id = last_updates['message']['chat']['id']
+    text = last_updates['message']['text']
+    message_id = last_updates['message']['message_id']
+    return chat_id, text, message_id
+
+def send_message(TOKEN,chat_id, text):
+    button1 = {'text':'samarkand'}
+    button2 = {'text':'tashkent'}
+    
+    keyboard = [[button1, button2]]
+    reply_markup = {'keyboard':keyboard,'resize_keyboard':True}
+
+    data = {
+            'chat_id':chat_id,
+            'text':text,
+            'parse_mode':'HTML',
+            'reply_markup':reply_markup
+        }
+
+    r = requests.post(f'https://api.telegram.org/bot{TOKEN}/sendMessage',json=data)
+
+updates = get_updates(TOKEN)
+last_update = get_last_update(updates)
+chat_id, text, last_message_id = last_update
+
+weather_data = weather_api.get_data_city(key,text)
+weather_text = weather_api.sFormat(weather_data)
+
+send_message(TOKEN,chat_id,weather_text)
