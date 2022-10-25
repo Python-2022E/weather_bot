104b02248100337476e9883c6d6448babe55747e
Jalilov <javohirjalilov777@gmail.com>
2022-10-25 14:58:44 +0500

    make weather api

--- weather_api.py
+++ weather_api.py
@@ -13,5 +13,13 @@ def get_data_city(key:str, city:str)->dict:
     r = requests.get(url,params=payload)
     return r.json()
 
+def sFormat(data):
+    weather = data['weather'][0]['main']
+    description = data['weather'][0]['description']
+    temp = round(float(data['main']['temp']) - 273)
+    wind = data['wind']['speed']
+    text = f'Weather: {weather}\nTemprature: {temp}\nWind Speed: {wind}\nDescription: {description}'
+
+    return text
 # print(get_data_city(key,"samarkand"))
 
