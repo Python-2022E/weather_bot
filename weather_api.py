6b1b4aca455569a17dff74be172edcc64049b208
Jalilov <javohirjalilov777@gmail.com>
2022-10-25 15:05:45 +0500

    weather api send other city

--- weather_api.py
+++ weather_api.py
@@ -4,22 +4,36 @@ import os
 key = os.environ['KEY']
 def get_data_city(key:str, city:str)->dict:
     url = f'https://api.openweathermap.org/data/2.5/weather'
-    
+
     payload = {
         'q':city,
         'appid':key
     }
     
     r = requests.get(url,params=payload)
-    return r.json()
+    if r.status_code == 200:
+        data = r.json()
+        weather = data['weather'][0]['main']
+        description = data['weather'][0]['description']
+        temp = round(float(data['main']['temp']) - 273)
+        wind = data['wind']['speed']
+        
+        text = f'Weather: {weather}\nTemprature: {temp}\nWind Speed: {wind}\nDescription: {description}'
+        return text
+    else:
+        return 'Quyidagi shaharlardan birini tanlang!👇'
 
-def sFormat(data):
-    weather = data['weather'][0]['main']
-    description = data['weather'][0]['description']
-    temp = round(float(data['main']['temp']) - 273)
-    wind = data['wind']['speed']
-    text = f'Weather: {weather}\nTemprature: {temp}\nWind Speed: {wind}\nDescription: {description}'
+# def sFormat(data):
+#     try:
+#         weather = data['weather'][0]['main']
+#         description = data['weather'][0]['description']
+#         temp = round(float(data['main']['temp']) - 273)
+#         wind = data['wind']['speed']
+#         text = f'Weather: {weather}\nTemprature: {temp}\nWind Speed: {wind}\nDescription: {description}'
+#         return text
+#     except:
+#         return data
 
-    return text
+    
 # print(get_data_city(key,"samarkand"))
 
